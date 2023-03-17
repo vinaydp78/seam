f4874b8fb2ce946b4fc80d3448808acf0c6dbb6b
VinayPatangedentsu <Vinay.Patange1@dentsu.com>
2023-03-17 11:44:50 +0530

    ami

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_nat_gateway" "my_nat_gateway" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0d718c3d715cec4a7"
+  #ami = "ami-0d718c3d715cec4a7"
   #ami = data.aws_ami.example.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
